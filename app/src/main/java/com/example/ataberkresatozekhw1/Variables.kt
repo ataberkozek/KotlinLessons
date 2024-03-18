a6129c60b508f3c62f117843fb79011cb52236fd
ataberkozek <ataberk.r.ozek@gmail.com>
2024-03-18 13:53:47 +0300

    initial commit

--- app/src/main/java/com/example/ataberkresatozekhw1/Variables.kt
+++ app/src/main/java/com/example/ataberkresatozekhw1/Variables.kt
@@ -1,5 +1,52 @@
 package com.example.ataberkresatozekhw1
 
 fun main() {
+    val ilce = "Kadikoy"
+    val kita = "Asya"
+    val faks = 2166543211
+    val pkodu = 34710
+    val insta = "instagram.com/ataberk"
+    val departman = "Ödeme sistemleri"
+    val urunMiktar = 100
+    val musteriSoyadi = "Ozek"
+    val odemeMiktar = 1000
+    val dogumTarihi = "12.01.1997"
+    val borc = 2000
+    val medeniHal = "Bekar"
+    val yorum = "Tesekkurler"
+    val odemeSaati = "12:00"
+    val eftMiktar = 500
+    val satilanMiktar = 50
+    val telefonModel = "Samsung"
+    val dergiAdi = "National Geographic"
+    val basimTarihi = "12.01.2022"
+    val zamMiktar = 0.55
+    val daireSayisi = 2
+    val enlem = 40.987654
+    val boylam = 29.123456
+    val yemekAdi = "Kuru Fasulye"
+    val urumFiyati = 45
+    val sirketAdi = "Apple"
+    val videoAdi = "Supermarkette 1 gece kaldım"
+    val muzikSuresi = 3.5
+    val mekanPuani = 4.5
+    val dosyaAdi = "resim.jpg"
+    val resimFormati = "jpg"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val bilgisayarModeli = "Macbook Pro"
+    val ekranBoyutu = 16.9
+    val kullanimSuresi = 5
+    val basinc = 1013
+    val etkinlikGunu = "18.03.2024"
+    val odemeGunu = "21.03.2024"
+    val yolculukCikisTarihi = "21.03.2024"
+    val mahalleAdi = "Caddebostan"
+    val otobusHatti = "122"
+    val kullanilanDakika = 100
+    val kargoTakipNo = "1234567890"
+    val kuponSuresi = 4
+    val kuponKodu = "ABCD1234"
+    val faturaTarihi = "21.03.2024"
 
 }
